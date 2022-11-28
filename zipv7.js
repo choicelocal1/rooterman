7b114259d64e88114743f81de0391aa89ce398ba
choicelocal1 <96065693+choicelocal1@users.noreply.github.com>
2022-11-28 16:49:59 -0500

    Update zipv7.js

--- zipv7.js
+++ zipv7.js
@@ -1,6 +1,6 @@
 Webflow.push(function() {
     $("#wf-form-Zip-Form").submit(function() {
-       var zip = document.getElementById("zip").value;
+       var zip = document.getElementById("zip-2").value;
         $(document).ajaxComplete(function() {
             var url = "https://www.rooterman.com/area-served/";
 location.href = url.concat(zip);    });
